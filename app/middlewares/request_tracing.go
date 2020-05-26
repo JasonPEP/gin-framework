916b1824e5b7feb1b30eef3b8bd0a993f5f25fbb
JasonPEP <jasonsuler@gmail.com>
2020-05-26 15:55:14 +0800

    Fix time api of use.

--- app/middlewares/request_tracing.go
+++ app/middlewares/request_tracing.go
@@ -24,7 +24,7 @@ func RequestTracing(c *gin.Context) {
 	couting(c)
 	begin := time.Now()
 	c.Next()
-	end := time.Now().Sub(begin)
+	end := time.Since(begin)
 	consuming(c, end)
 	log.Fatal(end.Milliseconds())
 }
